6d67fe781f109e07d4b91f33bf3eeb8ff5a5ffd8
letienhoa <55196825+letienhoa@users.noreply.github.com>
2019-10-04 09:10:15 +0700

    Add files via upload

---
+++ gamecaro/gamecaro/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gamecaro
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        #region Properties
+        banco bancaro;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+         bancaro = new banco(pnl);
+           
+             bancaro.vebanco();
+        }
+    }
+}
