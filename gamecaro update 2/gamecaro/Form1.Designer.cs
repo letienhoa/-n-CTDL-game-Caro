ead6680d837bf90182ea484ae2e8bc3601711470
letienhoa <55196825+letienhoa@users.noreply.github.com>
2019-12-11 23:53:04 +0700

    Add files via upload

---
+++ gamecaro update 2/gamecaro/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace gamecaro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnl = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.pnl.Location = new System.Drawing.Point(14, 14);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(686, 609);
+            this.pnl.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(707, 281);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(295, 332);
+            this.panel1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button3.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(153, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 44);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "EXITS";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(24, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "UNDO";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(24, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PLAY VS BOT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl1.BackColor = System.Drawing.Color.White;
+            this.lbl1.Font = new System.Drawing.Font("Broadway", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lbl1.Location = new System.Drawing.Point(724, 5);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(262, 68);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "GAME CARO";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = global::gamecaro.Properties.Resources.unnamed__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(707, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(295, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button4.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(24, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 48);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "PLAYER1 VS PLAYER2";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1014, 621);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnl);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CARO";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ gamecaro update 2/gamecaro/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gamecaro
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+       
+        #region Properties
+        banco bancaro;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            DialogResult dialogResult = MessageBox.Show("Bạn muốn chơi trước không", "Lượt chơi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+             
+                bancaro = new banco(pnl);
+                bancaro.chedochoi = 1;
+                bancaro.Luotchoi = 0;
+                bancaro.vebanco();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                
+                bancaro = new banco(pnl);
+                bancaro.chedochoi = 1;
+                bancaro.Luotchoi = 1;
+                bancaro.vebanco();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            bancaro.Undo();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            DialogResult ret = MessageBox.Show("Do you want exits ?", "EXITS",
+                         MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (ret == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            bancaro = new banco(pnl);
+            bancaro.chedochoi = 2;
+            bancaro.vebanco();
+        }
+    }
+    
+}
